fd293a090dfceae9211863627f8204dc11f84744
L3D <l3d@c3woc.de>
2023-10-02 01:16:25 +0200

    ENV variable testing

--- ansible-docker.py
+++ ansible-docker.py
@@ -1,4 +1,18 @@
 import subprocess
+import os
+
+# Specify the name of the environment variable you want to read
+env_var_name = "TEST"
+
+# Attempt to read the environment variable
+env_var_value = os.getenv(env_var_name)
+
+# Check if the environment variable exists
+if env_var_value is not None:
+    print(f"The value of {env_var_name} is: {env_var_value}")
+else:
+    print(f"The environment variable {env_var_name} is not set.")
+
 
 # Command to run
 command = ["ansible", "--version"]
